3e8502b7e3fce78f7a69a8a367e4fc0b0aa83a9c
Ngu Nguyen (Matthew) <thanhngu.nguyen@gumiviet.com>
2023-01-04 11:49:24 +0700

    Extract ticket ordering to Concert model.

--- app/Concert.php
+++ app/Concert.php
@@ -7,31 +7,45 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Concert extends Model
 {
-  protected $guarded = [];
-  protected $dates = ['date'];
+    protected $guarded = [];
+    protected $dates = ['date'];
 
-  public function scopePublished($query)
-  {
+    public function scopePublished($query)
+    {
     return $query->whereNotNull('published_at');
-  }
+    }
 
-  public function getFormattedDateAttribute()
-  {
+    public function getFormattedDateAttribute()
+    {
     return $this->date->format('F j, Y');
-  }
+    }
 
-  public function getFormattedStartTimeAttribute()
-  {
+    public function getFormattedStartTimeAttribute()
+    {
     return $this->date->format('g:ia');
-  }
+    }
 
-  public function getTicketPriceInDollarsAttribute()
-  {
+    public function getTicketPriceInDollarsAttribute()
+    {
     return number_format($this->ticket_price / 100, 2);
-  }
+    }
 
-  public function orders(): HasMany
-  {
+    public function orders(): HasMany
+    {
     return $this->hasMany(Order::class);
-  }
+    }
+
+    public function orderTickets($email, $ticketQuantity): Model
+    {
+        $order = $this->orders()->create([
+            'email' => $email
+        ]);
+
+        foreach (range(1, $ticketQuantity) as $i)
+        {
+            $order->tickets()->create([]);
+        }
+
+        return $order;
+    }
 }
