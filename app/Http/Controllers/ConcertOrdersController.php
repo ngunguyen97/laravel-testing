5248fdcac50b6ab82e7e43c616fa2148fe3319b8
Ngu Nguyen (Matthew) <thanhngu.nguyen@gumiviet.com>
2023-01-05 09:24:14 +0700

    Email is required to purchase tickets

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -17,6 +17,10 @@ class ConcertOrdersController extends Controller
 
     public function store($concertId)
     {
+        $this->validate(request(), [
+            'email' => 'required'
+        ]);
+
         $concert = Concert::find($concertId);
         $ticketQuantity = request('ticket_quantity');
         $amount = $ticketQuantity * $concert->ticket_price;
